28cc6a5ad0dbf9807e6180bb707c6472b63f5745
tcbalch07 <150624554+tcbalch07@users.noreply.github.com>
2024-09-15 17:15:17 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    current_date = datetime.now().strftime("%B %d, %Y")
+    return render_template('index.html', current_date=current_date)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/exam')
+def exam():
+    return render_template('exam.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
